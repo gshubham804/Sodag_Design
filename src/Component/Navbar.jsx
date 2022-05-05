def4526bf9105dee2f955371a01a64700ca149c4
SHUBHAM GUPTA <gshubham804@gmail.com>
2022-05-06 04:12:33 +0530

    Nav updated

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,40 +1,27 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../Assests/logo.png'
  
 const Navbar = () => {
   return (
     <>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-main-cont">
-  <div className="navbar-brand navbar-brand-img">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <div class="BG">
+    <nav>
+      <input type="checkbox" id="check"/>
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <label class="logo"><div className="nav-logo"></div></label>
+
+      <ul>
+        <li><a class="active" href="#">HOME</a></li>
+        <li><a href="#">PRODUCTS</a></li>
+        <li><a href="#">ABOUT</a></li>
+        <li className='last-option'><a href="#">CONTACT US</a></li>
+      </ul>
+    </nav>
+
   </div>
-
-  <div class="collapse navbar-collapse navbar-li-cont" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-        
-    <li class="nav-item active li-slant">
-        <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
-      </li>
-
-      <li class="nav-item active li-slant">
-        <a class="nav-link" href="#">Products <span class="sr-only">(current)</span></a>
-      </li>
-
-      <li class="nav-item active li-slant">
-        <a class="nav-link" href="#">About<span class="sr-only">(current)</span></a>
-      </li>
-
-      <li class="nav-item active li-onesideslant">
-        <a class="nav-link" href="#">Contact us <span class="sr-only">(current)</span></a>
-      </li>
-      
-      
-     
-    </ul>
-  </div>
-</nav>
     </>
   )
 }
