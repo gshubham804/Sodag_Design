462e2ab0a9714ac3fb2642be822566705d1d0fe2
SHUBHAM GUPTA <gshubham804@gmail.com>
2022-05-11 10:13:38 +0530

    updated 1.0

--- src/Component/Sodagall.jsx
+++ src/Component/Sodagall.jsx
@@ -1,20 +1,40 @@
 import React from "react";
 import Sodagall from "./Sodagall.css";
 import SodagallSlider from "./SodagallSlider";
+import image1 from '../Assests/LEMON.png'
+import image2 from '../Assests/JEERA.png'
+import image3 from '../Assests/REGULAR.png'
 
 const Sodag = () => {
   return (
     <>
       <div className="sodagall-main-cont">
+        <div className="sodagall-top">
+          <h5>Soda G All Products</h5>
+          <p>by Welable Pharma</p>
+        </div>
 
-          <div className="sodagall-top">
-              <h5>Soda G All Products</h5>
-              <p>by Welable Pharma</p>
+        <div className="sodagall-mix">
+          <div className="sodagall-mix-first"></div>
+          <div className="sodagall-mix-bgimg">
+                    <img src={image1} alt="" />
+                    <img src={image2} alt="" />
+                    <img src={image3} alt="" />
+                </div>
+          <div className="sodagall-mix-second">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab natus
+              hic vero sit consectetur modi.
+            </p>
           </div>
-          <SodagallSlider/>
+        </div>
+
+        <div className="cut-polygon"></div>
+
+        <SodagallSlider />
         <div className="sodagall-hor">
           <div className="sodag-flat-line">
-              <h5>How Soda-G Works?</h5>
+            <h5>How Soda-G Works?</h5>
           </div>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quia
@@ -22,7 +42,7 @@ const Sodag = () => {
             eaque nobis optio totam blanditiis, eligendi aliquam quo. Odio?
           </p>
           <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quia
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quia
             consequatur a ab error doloribus quos autem, ipsum alias, non et
             eaque nobis optio totam blanditiis, eligendi aliquam quo. Odio?
           </p>
@@ -30,7 +50,7 @@ const Sodag = () => {
 
         <div className="sodagall-hor">
           <div className="sodag-flat-line">
-              <h5>How Soda-G Works?</h5>
+            <h5>How Soda-G Works?</h5>
           </div>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quia
@@ -38,7 +58,7 @@ const Sodag = () => {
             eaque nobis optio totam blanditiis, eligendi aliquam quo. Odio?
           </p>
           <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quia
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quia
             consequatur a ab error doloribus quos autem, ipsum alias, non et
             eaque nobis optio totam blanditiis, eligendi aliquam quo. Odio?
           </p>
@@ -46,7 +66,7 @@ const Sodag = () => {
 
         <div className="sodagall-hor">
           <div className="sodag-flat-line">
-              <h5>How Soda-G Works?</h5>
+            <h5>How Soda-G Works?</h5>
           </div>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quia
@@ -54,7 +74,7 @@ const Sodag = () => {
             eaque nobis optio totam blanditiis, eligendi aliquam quo. Odio?
           </p>
           <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quia
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quia
             consequatur a ab error doloribus quos autem, ipsum alias, non et
             eaque nobis optio totam blanditiis, eligendi aliquam quo. Odio?
           </p>
@@ -62,18 +82,18 @@ const Sodag = () => {
 
         <div className="sodagall-hor">
           <div className="sodag-flat-line">
-              <h5>How Soda-G Works?</h5>
+            <h5>How Soda-G Works?</h5>
           </div>
-          
+
           <li>Lorem ipsum dolor sit amet.</li>
           <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
           <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
           <li>Lorem ipsum dolor sit amet consectetur.</li>
           <li>Lorem ipsum dolor sit amet.</li>
-          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, unde.</li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A, unde.
+          </li>
         </div>
-
-
       </div>
     </>
   );
